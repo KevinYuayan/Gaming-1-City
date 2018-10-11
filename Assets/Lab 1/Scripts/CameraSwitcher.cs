43ff37f5dcc97ec9368a7ef3365dc15dc2784a2e
jyuayan <300799109@cencol.ca>
2018-10-11 16:01:17 -0400

    first commit

---
+++ Assets/Lab 1/Scripts/CameraSwitcher.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+public class CameraSwitcher : MonoBehaviour
+{
+    public Camera[] cameras = new Camera[4];
+    public bool changeAudioListener = true;
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            if (cameras[0].enabled)
+            {
+                EnableCamera(cameras[0], false);
+            }
+            else
+            {
+                EnableCamera(cameras[0], true);
+            }
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            if (cameras[1].enabled)
+            {
+                EnableCamera(cameras[1], false);
+            }
+            else
+            {
+                EnableCamera(cameras[1], true);
+            }
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            if (cameras[2].enabled)
+            {
+                EnableCamera(cameras[2], false);
+            }
+            else
+            {
+                EnableCamera(cameras[2], true);
+            }
+        }
+        if (Input.GetKeyDown("m"))
+        {
+            if (cameras[3].enabled)
+            {
+                EnableCamera(cameras[3], false);
+            }
+            else
+            {
+                EnableCamera(cameras[3], true);
+            }
+        }
+    }
+    private void EnableCamera(Camera cam, bool
+    enabledStatus)
+    {
+        cam.enabled = enabledStatus;
+        if (changeAudioListener)
+            cam.GetComponent<AudioListener>().enabled =
+            enabledStatus;
+    }
+}
---
+++ Assets/Lab 1/Scripts/SceneCapture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneCapture : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            int fileNumber = 1;
+            while (System.IO.File.Exists("ScreenShots/" + fileNumber + ".png"))
+            {
+                fileNumber++;
+            }
+            ScreenCapture.CaptureScreenshot("ScreenShots/" + fileNumber + ".png");
+        }
+    }
+}
